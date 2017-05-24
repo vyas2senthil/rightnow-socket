ad6a598b36f5567bc0db517b10be838817dedf8e
Marco Abate <marcoabate1984@gmail.com>
2017-05-24 09:45:15 +0200

    Test with another action emitted

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+    console.log('Client connected');
+    io.emit('connection', JSON.stringify(socket))
+    socket.on('disconnect', () => {
+        console.log('Client disconnected')
+    });
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 10000);
